4c3aa3212ae3b0a7ce6e2dd8cbb792ffad2f7bad
engrh <engrh@LAPTOP-3U6QD2HI.lan>
2020-05-31 22:26:17 +0200

    added a unit test

--- assignment/src/test/java/assignment/ConverterTest.java
+++ assignment/src/test/java/assignment/ConverterTest.java
@@ -2,6 +2,8 @@ package assignment;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -13,6 +15,12 @@ public class ConverterTest {
 		int value = 11209;
 		StringConverter converter = new StringConverter();
 		Assert.assertEquals("ali", converter.NumberToString(value));
+	}
+	@Test
+	public void boundaryConverter() {
+		int value = new Random().nextInt(999999);
+		StringConverter converter = new StringConverter();
+		Assert.assertNotNull(converter.NumberToString(value));
 	}
 
 }
